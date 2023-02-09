3255ec38b182b9394a99146931c96ffde6264150
DanielCurtis1 <dwcurti1@stu.k12.wv.us>
2023-02-09 08:35:39 -0500

    added notes and replaced cone/cube pipeline.

--- vision.py
+++ vision.py
@@ -7,8 +7,16 @@ from pipeline.example import ExamplePipeline
 ####IMPORTANT NOTES####
 #index of 0 = shape
 #index of 1 = notshape
+
 #using index for if statements is much easier than using class_name[] or class_names[]
 #as class_names[] has both a number and a name while class_name[] is just weird. 
+
+#TO DO: CHANGE EXAMPLE PIPELINE TO DUAL PIPELINE IN LINE 60 (when dual pipeline is actually created, that is).
+
+#make sure to check example pipeline to see what changes that need to be made to make dual pipeline work.
+
+#TO DO: CHANGE ALL PIPELINES TO JAVA, NETWORK TABLES ARE WEIRD IN PYTHON. (if it can still keep up with only using python)
+#ignore that to do if pipelines are easier than I thought.
 #######################
 
 
@@ -26,9 +34,13 @@ class_names = open("modeldata\labels.txt", "r").readlines()
 # CAMERA can be 0 or 1 based on default camera of your computer
 camera = cv2.VideoCapture(0)
 
+# Initiate the pipeline
+pipe = ExamplePipeline()
+
 while True:
     # Grab the webcamera's image.
     ret, image = camera.read()
+    ret, reimage = camera.read()
 
     # Resize the raw image into (224-height,224-width) pixels
     image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
@@ -50,10 +62,18 @@ while True:
 
     # Testing class_name usage
     if index == 1: #nonshape detected
-        ExamplePipeline.calledexample(camera)
+
+        print("no shape was detected", end="\n")
+
     elif index == 0: #shape destected
-        print("sadge", end="\n")
+        
+        # You could technically use 'image' here but since it was resize you would need to undo that first
+        # I found it easier just to make a duplicate without the resizing: 'reimage'.
+        pipe.process(reimage)
+        print(pipe.filter_contours_0_output)
+
     else: #error occured
+
         print("prediction or camera failed.", end="\n")
 
     # Print prediction and confidence score
