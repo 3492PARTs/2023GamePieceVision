29cd5e395f7a187c4707165245f0e440668829e6
DanielCurtis1 <dwcurti1@stu.k12.wv.us>
2023-02-10 15:13:40 -0500

    holy moly a lot of stuff

--- pipeline/dualpipeline.py
+++ pipeline/dualpipeline.py
@@ -60,76 +60,42 @@ class DualPipeline:
 
         self.find_contours_1_output = None
 
-        self.__filter_contours_0_contours = self.find_contours_0_output
-        self.__filter_contours_0_min_area = 100.0
-        self.__filter_contours_0_min_perimeter = 0.0
-        self.__filter_contours_0_min_width = 0.0
-        self.__filter_contours_0_max_width = 1000.0
-        self.__filter_contours_0_min_height = 0.0
-        self.__filter_contours_0_max_height = 1000.0
-        self.__filter_contours_0_solidity = [0, 100]
-        self.__filter_contours_0_max_vertices = 1000000.0
-        self.__filter_contours_0_min_vertices = 0.0
-        self.__filter_contours_0_min_ratio = 0.0
-        self.__filter_contours_0_max_ratio = 1000.0
-
-        self.filter_contours_0_output = None
-
-        self.__filter_contours_1_contours = self.find_contours_1_output
-        self.__filter_contours_1_min_area = 700.0
-        self.__filter_contours_1_min_perimeter = 0
-        self.__filter_contours_1_min_width = 0
-        self.__filter_contours_1_max_width = 1000
-        self.__filter_contours_1_min_height = 0
-        self.__filter_contours_1_max_height = 1000
-        self.__filter_contours_1_solidity = [0, 100]
-        self.__filter_contours_1_max_vertices = 1000000
-        self.__filter_contours_1_min_vertices = 0
-        self.__filter_contours_1_min_ratio = 0
-        self.__filter_contours_1_max_ratio = 1000
-
-        self.filter_contours_1_output = None
+        
 
 
-    def process(self, source0):
+    def process(self, source0, gametype):
         """
         Runs the pipeline and sets all outputs to new values.
         """
+        
         # Step Resize_Image0:
         self.__resize_image_input = source0
         (self.resize_image_output) = self.__resize_image(self.__resize_image_input, self.__resize_image_width, self.__resize_image_height, self.__resize_image_interpolation)
 
-        # Step HSV_Threshold0:
-        self.__hsv_threshold_0_input = self.resize_image_output
-        (self.hsv_threshold_0_output) = self.__hsv_threshold(self.__hsv_threshold_0_input, self.__hsv_threshold_0_hue, self.__hsv_threshold_0_saturation, self.__hsv_threshold_0_value)
+        if gametype == 0:
+            # Step HSV_Threshold0:
+            self.__hsv_threshold_0_input = self.resize_image_output
+            (self.hsv_threshold_0_output) = self.__hsv_threshold(self.__hsv_threshold_0_input, self.__hsv_threshold_0_hue, self.__hsv_threshold_0_saturation, self.__hsv_threshold_0_value)
 
-        # Step HSV_Threshold1:
-        self.__hsv_threshold_1_input = self.resize_image_output
-        (self.hsv_threshold_1_output) = self.__hsv_threshold(self.__hsv_threshold_1_input, self.__hsv_threshold_1_hue, self.__hsv_threshold_1_saturation, self.__hsv_threshold_1_value)
+            # Step CV_erode0:
+            self.__cv_erode_0_src = self.hsv_threshold_0_output
+            (self.cv_erode_0_output) = self.__cv_erode(self.__cv_erode_0_src, self.__cv_erode_0_kernel, self.__cv_erode_0_anchor, self.__cv_erode_0_iterations, self.__cv_erode_0_bordertype, self.__cv_erode_0_bordervalue)
 
-        # Step CV_erode0:
-        self.__cv_erode_0_src = self.hsv_threshold_0_output
-        (self.cv_erode_0_output) = self.__cv_erode(self.__cv_erode_0_src, self.__cv_erode_0_kernel, self.__cv_erode_0_anchor, self.__cv_erode_0_iterations, self.__cv_erode_0_bordertype, self.__cv_erode_0_bordervalue)
+            # Step Find_Contours0:
+            self.__find_contours_0_input = self.cv_erode_0_output
+            (self.find_contours_0_output) = self.__find_contours(self.__find_contours_0_input, self.__find_contours_0_external_only)
+        else:
+            # Step HSV_Threshold1:
+            self.__hsv_threshold_1_input = self.resize_image_output
+            (self.hsv_threshold_1_output) = self.__hsv_threshold(self.__hsv_threshold_1_input, self.__hsv_threshold_1_hue, self.__hsv_threshold_1_saturation, self.__hsv_threshold_1_value)
 
-        # Step CV_erode1:
-        self.__cv_erode_1_src = self.hsv_threshold_1_output
-        (self.cv_erode_1_output) = self.__cv_erode(self.__cv_erode_1_src, self.__cv_erode_1_kernel, self.__cv_erode_1_anchor, self.__cv_erode_1_iterations, self.__cv_erode_1_bordertype, self.__cv_erode_1_bordervalue)
+            # Step CV_erode1:
+            self.__cv_erode_1_src = self.hsv_threshold_1_output
+            (self.cv_erode_1_output) = self.__cv_erode(self.__cv_erode_1_src, self.__cv_erode_1_kernel, self.__cv_erode_1_anchor, self.__cv_erode_1_iterations, self.__cv_erode_1_bordertype, self.__cv_erode_1_bordervalue)
 
-        # Step Find_Contours0:
-        self.__find_contours_0_input = self.cv_erode_0_output
-        (self.find_contours_0_output) = self.__find_contours(self.__find_contours_0_input, self.__find_contours_0_external_only)
-
-        # Step Find_Contours1:
-        self.__find_contours_1_input = self.cv_erode_1_output
-        (self.find_contours_1_output) = self.__find_contours(self.__find_contours_1_input, self.__find_contours_1_external_only)
-
-        # Step Filter_Contours0:
-        self.__filter_contours_0_contours = self.find_contours_0_output
-        (self.filter_contours_0_output) = self.__filter_contours(self.__filter_contours_0_contours, self.__filter_contours_0_min_area, self.__filter_contours_0_min_perimeter, self.__filter_contours_0_min_width, self.__filter_contours_0_max_width, self.__filter_contours_0_min_height, self.__filter_contours_0_max_height, self.__filter_contours_0_solidity, self.__filter_contours_0_max_vertices, self.__filter_contours_0_min_vertices, self.__filter_contours_0_min_ratio, self.__filter_contours_0_max_ratio)
-
-        # Step Filter_Contours1:
-        self.__filter_contours_1_contours = self.find_contours_1_output
-        (self.filter_contours_1_output) = self.__filter_contours(self.__filter_contours_1_contours, self.__filter_contours_1_min_area, self.__filter_contours_1_min_perimeter, self.__filter_contours_1_min_width, self.__filter_contours_1_max_width, self.__filter_contours_1_min_height, self.__filter_contours_1_max_height, self.__filter_contours_1_solidity, self.__filter_contours_1_max_vertices, self.__filter_contours_1_min_vertices, self.__filter_contours_1_min_ratio, self.__filter_contours_1_max_ratio)
+            # Step Find_Contours1:
+            self.__find_contours_1_input = self.cv_erode_1_output
+            (self.find_contours_1_output) = self.__find_contours(self.__find_contours_1_input, self.__find_contours_1_external_only)
 
 
     @staticmethod
@@ -189,71 +155,29 @@ class DualPipeline:
             mode = cv2.RETR_LIST
         method = cv2.CHAIN_APPROX_SIMPLE
         contours, hierarchy =cv2.findContours(input, mode=mode, method=method)
+        ###END GRIP STUFF###
         
-        
+        #own stuff
+
+        #finds width, height, the and the coordinates of one of the verticies
         if contours:
+
+            
+
             c = max(contours, key=cv2.contourArea)
 
+            #creates a bounding rect, finds the center of the rect for rotation
+            
             x, y, w, h = cv2.boundingRect(c)
             centerw = w / 2
-            centerw = int(centerw)
             centerh = h / 2
-            centerh = int(centerh)
-            print(str(centerw) +" "+ str(centerh))
+            area = w*h
 
-            #M = cv2.moments(c)
-        
-            #if M["m00"] != 0:
-            #    x = int(M["m10"] / M["m00"])
-            #    y = int(M["m01"] / M["m00"])
-            #    print(str(x) + " " + str(y))
-        return contours
+            #draws a rectangle onto "input", where (x,y) are one vertice, and (x+width, y+height) is the opposite one.
+            cv2.rectangle(input,(x,y),(x+w,y+h),(135,50,30),3)
+            cv2.circle(input, (int(x+centerw),int(y+centerh)),5,(135,50,30),-1)
+            cv2.imshow("result",input)
 
-    @staticmethod
-    def __filter_contours(input_contours, min_area, min_perimeter, min_width, max_width,
-                        min_height, max_height, solidity, max_vertex_count, min_vertex_count,
-                        min_ratio, max_ratio):
-        """Filters out contours that do not meet certain criteria.
-        Args:
-            input_contours: Contours as a list of numpy.ndarray.
-            min_area: The minimum area of a contour that will be kept.
-            min_perimeter: The minimum perimeter of a contour that will be kept.
-            min_width: Minimum width of a contour.
-            max_width: MaxWidth maximum width.
-            min_height: Minimum height.
-            max_height: Maximimum height.
-            solidity: The minimum and maximum solidity of a contour.
-            min_vertex_count: Minimum vertex Count of the contours.
-            max_vertex_count: Maximum vertex Count.
-            min_ratio: Minimum ratio of width to height.
-            max_ratio: Maximum ratio of width to height.
-        Returns:
-            Contours as a list of numpy.ndarray.
-        """
-        
-        output = []
-        for contour in input_contours:
-            x,y,w,h = cv2.boundingRect(contour)
-            if (w < min_width or w > max_width):
-                continue
-            if (h < min_height or h > max_height):
-                continue
-            area = cv2.contourArea(contour)
-            if (area < min_area):
-                continue
-            if (cv2.arcLength(contour, True) < min_perimeter):
-                continue
-            hull = cv2.convexHull(contour)
-            solid = 100 * area / cv2.contourArea(hull)
-            if (solid < solidity[0] or solid > solidity[1]):
-                continue
-            if (len(contour) < min_vertex_count or len(contour) > max_vertex_count):
-                continue
-            ratio = (float)(w) / h
-            if (ratio < min_ratio or ratio > max_ratio):
-                continue
-            output.append(contour)
-        return output
-
-
-
+            return centerh, centerw, x, y, w, h, area
+        else:
+            return None
--- vision.py
+++ vision.py
@@ -6,15 +6,9 @@ from pipeline import *
 from pipeline.dualpipeline import DualPipeline
 
 ####IMPORTANT NOTES####
-#index of 0 = shape
-#index of 1 = notshape
-
-#using index for if statements is much easier than using class_name[] or class_names[]
-#as class_names[] has both a number and a name while class_name[] is just weird. 
-
-#TO DO: CHANGE EXAMPLE PIPELINE TO DUAL PIPELINE IN LINE 60 (when dual pipeline is actually created, that is).
-
 #make sure to check example pipeline to see what changes that need to be made to make dual pipeline work.
+
+#under dualpipe.process(image, {}), the {} determines what game piece you search for, 0 for cone, anything else for cube
 
 #TO DO: CHANGE ALL PIPELINES TO JAVA, NETWORK TABLES ARE WEIRD IN PYTHON. (if it can still keep up with only using python)
 #ignore that to do if pipelines are easier than I thought.
@@ -26,12 +20,6 @@ from pipeline.dualpipeline import DualPipeline
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
 
-# Load the model
-model = load_model("modeldata\keras_model.h5", compile=False)
-
-# Load the labels
-class_names = open("modeldata\labels.txt", "r").readlines()
-
 # CAMERA can be 0 or 1 based on default camera of your computer
 camera = cv2.VideoCapture(0)
 
@@ -42,47 +30,16 @@ dualpipe = DualPipeline()
 while True:
     # Grab the webcamera's image.
     ret, image = camera.read()
-    ret, reimage = camera.read()
-
-    # Resize the raw image into (224-height,224-width) pixels
-    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
 
     # Show the image in a window
-    cv2.imshow("Webcam Image", image)
-
-    # Make the image a numpy array and reshape it to the models input shape.
-    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
-
-    # Normalize the image array
-    image = (image / 127.5) - 1
-
-    # Predicts the model
-    prediction = model.predict(image)
-    index = np.argmax(prediction)
-    class_name = class_names[index]
-    confidence_score = prediction[0][index]
-
-    # Testing class_name usage
-    if index == 1: #nonshape detected
-
-        print("no shape was detected", end="\n")
-
-    elif index == 0: #shape destected
-        
-        # You could technically use 'image' here but since it was resize you would need to undo that first
-        # I found it easier just to make a duplicate without the resizing: 'reimage'.
-        #pipe.process(reimage)
-        #print(pipe.filter_contours_0_output)
-        dualpipe.process(reimage)
-        #print(dualpipe.filter_contours_0_output)
-
-    else: #error occured
-
-        print("prediction or camera failed.", end="\n")
-
-    # Print prediction and confidence score
-    print("Class:", class_name[2:], end="")
-    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+    cv2.imshow("Plain Image", image)
+    
+    #pipe.process(reimage)
+    #print(pipe.filter_contours_0_output)
+    dualpipe.process(image, 0)
+    
+    if dualpipe.find_contours_0_output != None:
+        print(dualpipe.find_contours_0_output)
 
     # Listen to the keyboard for presses.
     keyboard_input = cv2.waitKey(1)
