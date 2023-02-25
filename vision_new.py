f4af92723ac53b13569829ae084c1986dc65789d
DanielCurtis1 <98223956+DanielCurtis1@users.noreply.github.com>
2023-02-25 10:39:49 -0500

    undo my fix, not fix

--- vision_new.py
+++ vision_new.py
@@ -1,42 +1,38 @@
 import cv2 as cv
 import numpy as np
 from pipeline import Pipeline
+import ntcore as networktables
 import os
 from os.path import basename
 import math
-from networktables import NetworkTables
 #uncomment this for the test server
 #import argparse
-#import ntcore as networktables
 
 
 ########################### NETWORK TABLES #########################
 
 
+
+instance = networktables.NetworkTableInstance.getDefault()
+
+identity = f"{basename(__file__)}-{os.getpid()}"
+instance.startClient4(identity)
+
+
 #### TEST SERVER ####
-#instance = networktables.NetworkTableInstance.getDefault()
-
-#identity = f"{basename(__file__)}-{os.getpid()}"
-#instance.startClient4(identity)
-
-
-
 #parser = argparse.ArgumentParser()
 #parser.add_argument("ip", type=str, help="IP address to connect to")
 #args = parser.parse_args()
 #instance.setServer(server_name=args.ip, port=networktables.NetworkTableInstance.kDefaultPort4)
-
-
-# Comment this out for the test server
-#instance.setServerTeam(team=3492, port=networktables.NetworkTableInstance.kDefaultPort4)
-
-#table = instance.getTable("vision")
-#distance = table.getFloatTopic("distance").publish()
-#pixels = table.getFloatTopic("pixels").publish()
-#angle = table.getFloatTopic("angle").publish()
 #####################
 
-nt = NetworkTables.getTable('vision')
+# Comment this out for the test server
+instance.setServerTeam(team=3492, port=networktables.NetworkTableInstance.kDefaultPort4)
+
+table = instance.getTable("vision")
+distance = table.getFloatTopic("distance").publish()
+pixels = table.getFloatTopic("pixels").publish()
+angle = table.getFloatTopic("angle").publish()
 
 ###################################################################
 
@@ -90,33 +86,33 @@ def findDistanceAndPixels():
             if pipeCube.extract_condata_1_output != None:
                 centerw = pipeCube.extract_condata_1_output[1]
                 difference_in_pix_x = centerw - 320
-                nt.putFloat('distance', float(pipeCube.find_distance_1_output))
-                nt.putFloat('pixels', float(difference_in_pix_x))
-                nt.putFloat('angle', float(calculateAngle(differenceInPixels=difference_in_pix_x)))
+                distance.set(value=float(pipeCube.find_distance_1_output))
+                pixels.set(value=float(difference_in_pix_x))
+                angle.set(value=float(calculateAngle(differenceInPixels=difference_in_pix_x)))
 
         else:
             if pipeCone.extract_condata_0_output != None:
                 centerw = pipeCone.extract_condata_0_output[1]
                 difference_in_pix_x = centerw - 320
-                nt.putFloat('distance', float(pipeCone.find_distance_0_output))
-                nt.putFloat('pixels', float(difference_in_pix_x))
-                nt.putFloat('angle', float(calculateAngle(differenceInPixels=difference_in_pix_x)))
+                distance.set(value=float(pipeCone.find_distance_0_output))
+                pixels.set(value=float(difference_in_pix_x))
+                angle.set(value=float(calculateAngle(differenceInPixels=difference_in_pix_x)))
     
     if pipeCube.find_distance_1_output != None and pipeCone.find_contours_0_output == None:
         if pipeCube.extract_condata_1_output != None:
             centerw = pipeCube.extract_condata_1_output[1]
             difference_in_pix_x = centerw - 320
-            nt.putFloat('distance', float(pipeCube.find_distance_1_output))
-            nt.putFloat('pixels', float(difference_in_pix_x))
-            nt.putFloat('angle', float(calculateAngle(differenceInPixels=difference_in_pix_x)))
+            distance.set(value=float(pipeCube.find_distance_1_output))
+            pixels.set(value=float(difference_in_pix_x))
+            angle.set(value=float(calculateAngle(differenceInPixels=difference_in_pix_x)))
     
     if pipeCone.find_distance_0_output != None and pipeCube.find_distance_1_output == None:    
         if pipeCone.extract_condata_0_output != None:
             centerw = pipeCone.extract_condata_0_output[1]
             difference_in_pix_x = centerw - 320
-            nt.putFloat('distance', float(pipeCone.find_distance_0_output))
-            nt.putFloat('pixels', float(difference_in_pix_x))
-            nt.putFloat('angle', float(calculateAngle(differenceInPixels=difference_in_pix_x)))
+            distance.set(value=float(pipeCone.find_distance_0_output))
+            pixels.set(value=float(difference_in_pix_x))
+            angle.set(value=float(calculateAngle(differenceInPixels=difference_in_pix_x)))
 
 while True:
     ret, image = camera.read()
